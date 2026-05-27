2d53d5caae6d5a43d1ce13474acf6f8e8c9bc1d1
AlexandrAnatoliev <per-1986@list.ru>
2026-05-27 19:06:50 +0300

    fix: add php-mode

--- init.el
+++ init.el
@@ -2,3 +2,6 @@
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
+
+(require 'php-mode)
+(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
